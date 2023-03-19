766a0a0bbb73604832c922f2cedc8a768cb5c179
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-03-19 11:36:28 +0000

    complete all plus bonus goal

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    #Check for query parameter-name, case insensitive search
+    if request.query_parameters.key?("name")
+      name = request.query_parameters[:name]
+      students = Student.where("lower(first_name) = ?", name.downcase).or(Student.where("lower(last_name) = ?",name.downcase))
+      render(json: students)
+    else
+      students = Student.all
+      render json: students
+    end
   end
 
+  def show
+    render(json: Student.find(params[:id]))
+  end
 end
